6636b87731a7a06b10978361c5451023ed6d99cc
Ao-chi <106396818+Ao-chi@users.noreply.github.com>
2022-11-28 18:37:48 +0800

    Edit banner and toogle nav

--- scripts/index.js
+++ scripts/index.js
@@ -11,11 +11,26 @@ let toggleFunciton = () => {
     iconMenu.addEventListener("click", () => {
         nav.classList.toggle("active");
         overlay.classList.toggle("active");
+
+        if (iconClose.classList.contains("clicked")) {
+            iconClose.classList.remove("clicked");
+        } else {
+            iconClose.classList.add("clicked");
+            iconMenu.classList.add("hide");
+        }
     });
 
     iconClose.addEventListener("click", () => {
         nav.classList.toggle("active");
         overlay.classList.toggle("active");
+
+        if (iconClose.classList.contains("clicked")) {
+            iconClose.classList.remove("clicked");
+            iconMenu.classList.remove("hide");
+        } else {
+            iconClose.classList.add("clicked");
+            iconMenu.classList.remove("hide");
+        }
     });
 };
 
