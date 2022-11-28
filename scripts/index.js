cd4aacdfb8ce746f629dc0e163143bc0847efd1e
Ao-chi <106396818+Ao-chi@users.noreply.github.com>
2022-11-28 17:53:22 +0800

    grid and header edits

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,22 @@
 let nav = document.querySelector(".menu-items");
 let menuClose = document.querySelector(".menu_close");
-let menuOpen = document.querySelector(".menu_open");
+let toggleNav = document.querySelector(".nav-toggle");
+let iconNav = document.querySelector(".icon");
 let overlay = document.querySelector(".overlay");
 
-let isclicked = false;
-function nav_toggle() {
-    menuOpen.addEventListener("click", () => {
+let iconMenu = document.querySelector(".icon-menu");
+let iconClose = document.querySelector(".icon-close");
+// click function for nav toggle
+let toggleFunciton = () => {
+    iconMenu.addEventListener("click", () => {
         nav.classList.toggle("active");
         overlay.classList.toggle("active");
     });
-}
 
-nav_toggle();
+    iconClose.addEventListener("click", () => {
+        nav.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+};
+
+toggleFunciton();
